1f17876a570dca5572c3ec8b366f365d56ebbd80
LDananjaya <Dananjaya703@gmail.com>
2020-02-16 05:52:39 -0800

    firstcommit1

---
+++ helloworld/src/com/sliit/main/main.java
@@ -0,0 +1,11 @@
+package com.sliit.main;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is first project hello world");
+
+	}
+
+}
